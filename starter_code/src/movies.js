40877d062cb0155a6ed382235656c9f918a4506b
daniel vicario <danigv.email@gmail.com>
2019-06-18 22:41:19 +0200

    fixes by dani

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,66 +1,59 @@
-/* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-function turnHoursToMinutes (movies){
-  var dataToReturn =  movies.map((movie)=>{
-    var newDuration =  movie.duration.split(" ");
-    if(newDuration.length === 2){
-      newDuration = parseInt(newDuration[0])*60 + parseInt(newDuration[1])
-    }else{
-      if(newDuration[0].includes('h')){
-        newDuration = parseInt(newDuration[0])*60
-      }else{
+function turnHoursToMinutes(movies) {
+  let dataToReturn = movies.map((movie) => {
+    let newDuration = movie.duration.split(" ");
+    if (newDuration.length === 2) {
+      newDuration = parseInt(newDuration[0]) * 60 + parseInt(newDuration[1])
+    } else {
+      if (newDuration[0].includes('h')) {
+        newDuration = parseInt(newDuration[0]) * 60
+      } else {
         newDuration = parseInt(newDuration[0])
       }
     }
-    return {...movie, duration: newDuration}
+    return { ...movie, duration: newDuration }
   })
   return dataToReturn;
 }
-// 2h
-// 30min
-// 2h 30min
-
 
 // Get the average of all rates with 2 decimals 
-function ratesAverage(movies){
-  var averageToReturn = movies.reduce((acc, movie)=>{
-    if(movie.rate.length==0){
+function ratesAverage(movies) {
+  let averageToReturn = movies.reduce((acc, movie) => {
+    if (movie.rate.length == 0) {
       return acc
     }
     return acc += parseFloat(movie.rate)
-  },0)
+  }, 0)
   averageToReturn /= movies.length;
   averageToReturn = parseFloat(averageToReturn.toFixed(2))
   return averageToReturn;
 }
 
-
 // Get the average of Drama Movies
-function dramaMoviesRate(movies){
-  var dataToReturn = movies.filter((movie)=>{
+function dramaMoviesRate(movies) {
+  let dataToReturn = movies.filter((movie) => {
     return movie.genre.includes("Drama")
   })
-  if(dataToReturn.length === 0){
+  if (dataToReturn.length === 0) {
     return undefined
   }
   dataToReturn = ratesAverage(dataToReturn)
   return dataToReturn;
 }
 
-
 // Order by time duration, in growing order
-function orderByDuration(movies){
-  var dataToReturn = movies.sort((a,b)=>{
-    if(a.duration > b.duration){
+function orderByDuration(movies) {
+  let dataToReturn = movies.sort((a, b) => {
+    if (a.duration > b.duration) {
       return 1
     }
-    if(a.duration < b.duration){
+    if (a.duration < b.duration) {
       return -1
     }
-    if(a.title > b.title){
+    if (a.title > b.title) {
       return 1
     }
-    if(a.title < b.title){
+    if (a.title < b.title) {
       return -1
     }
     return 0
@@ -69,14 +62,14 @@ function orderByDuration(movies){
 }
 
 // How many movies did STEVEN SPIELBERG
-function howManyMovies(movies){
-  if(movies.length===0){
+function howManyMovies(movies) {
+  if (movies.length === 0) {
     return undefined;
   }
-  var dataToReturn = movies.filter((movie)=>{
+  let dataToReturn = movies.filter((movie) => {
     return movie.director == "Steven Spielberg"
   })
-  dataToReturn = dataToReturn.filter((movie)=>{
+  dataToReturn = dataToReturn.filter((movie) => {
     return movie.genre.includes("Drama")
   })
   dataToReturn = dataToReturn.length;
@@ -85,8 +78,8 @@ function howManyMovies(movies){
 }
 
 // Order by title and print the first 20 titles
-function orderAlphabetically(movies){
-  var dataToReturn = movies.map((movie)=>{
+function orderAlphabetically(movies) {
+  let dataToReturn = movies.map((movie) => {
     return movie.title
   })
   dataToReturn.sort()
@@ -94,44 +87,23 @@ function orderAlphabetically(movies){
 }
 
 // Best yearly rate average
-function bestYearAvg(movies){
- if(movies.length === 0){return;}
+function bestYearAvg(movies) {
+  if (movies.length === 0) { return; }
 
- var bestYear = 0, bestRate = 0;
+  let bestYear = 0, bestRate = 0;
   let moviesByYear = {};
   movies.forEach(movie => {
-    if(!moviesByYear[movie.year]){
-      moviesByYear[movie.year] =  [];
+    if (!moviesByYear[movie.year]) {
+      moviesByYear[movie.year] = [];
     }
     moviesByYear[movie.year].push(movie);
   });
-  for(let year in moviesByYear){
+  for (let year in moviesByYear) {
     let rate = ratesAverage(moviesByYear[year]);
-    if(rate > bestRate){
+    if (rate > bestRate) {
       bestRate = rate;
       bestYear = year;
     }
   }
   return (`The best year was ${bestYear} with an average rate of ${bestRate}`);
-
-  // if (movies.length===0){
-  //   return undefined
-  // }else{
-    
-  //   var bestYear = '';
-  //   var bestRate = 0
-  //   movies.forEach(function(movie){
-  //     var someYearMovies = movies.filter(function(movieFilter){
-  //       return movieFilter.year == movie.year;
-  //     });
-  //     var rate = ratesAverage(someYearMovies);
-  
-  //     if(rate >= bestRate){
-  //       bestRate = rate;
-  //       bestYear = movie.year;
-  //     }
-  //   });
-  //   return (`The best year was ${bestYear} with an average rate of ${bestRate}`);
-  // }
-  
 }
